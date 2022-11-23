b74fdc7cde06af396245bb3d9cb4b086991c455f
LuckMeelo <charmeel.vodouhe@epitech.eu>
2022-11-23 23:06:33 +0100

    feat: add commands sent by the brain

---
+++ src/commands/sentbybrain.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+def error(msg: str) -> None: print("ERROR - " + msg)
+def unknown(msg: str) -> None: print("UNKNOWN - " + msg)
+def debug(msg: str) -> None: print("DEBUG - " + msg)
+def message(msg: str) -> None: print("MESSAGE - " + msg)
